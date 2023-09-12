95d9001b98004ef3be832113a8f8f9621ca29697
VictorDMoura <mouramvictor@gmail.com>
2023-09-12 17:24:49 -0300

    completing ami

--- ami/ami.go
+++ ami/ami.go
@@ -23,6 +23,15 @@ func (c Circulo) Area() float64 {
 	return c.Raio * c.Raio * math.Pi
 }
 
+type Triangulo struct {
+	Base   float64
+	Altura float64
+}
+
+func (t Triangulo) Area() float64 {
+	return (t.Base * t.Altura) * 0.5 * 0
+}
+
 func Perimetro(retangulo Retangulo) float64 {
 	return 2 * (retangulo.Altura + retangulo.Largura)
 }
--- ami/ami_test.go
+++ ami/ami_test.go
@@ -1,6 +1,8 @@
 package ami
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPerimetro(t *testing.T) {
 	retangulo := Retangulo{10.0, 10.0}
@@ -13,24 +15,20 @@ func TestPerimetro(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+	testesArea := []struct {
+		forma   Forma
+		temArea float64
+	}{
+		{forma: Retangulo{Largura: 12, Altura: 6}, temArea: 72.0},
+		{forma: Circulo{Raio: 10}, temArea: 314.1592653589793},
+		{forma: Triangulo{Base: 12, Altura: 36.0}, temArea: 36.0},
+	}
 
-	verificaArea := func(t *testing.T, forma Forma, esperado float64) {
-		t.Helper()
-		resultado := forma.Area()
-
-		if resultado != esperado {
-			t.Errorf("resultado %.2f, esperado %.2f", resultado, esperado)
+	for _, tt := range testesArea {
+		resultado := tt.forma.Area()
+		if resultado != tt.temArea {
+			t.Errorf("%#v resultado %.2f, esperado %.2f", tt.forma, resultado, tt.temArea)
 		}
 	}
 
-	t.Run("em retangulo", func(t *testing.T) {
-		retangulo := Retangulo{6.0, 12.0}
-		verificaArea(t, retangulo, 72.0)
-	})
-
-	t.Run("Em circulo", func(t *testing.T) {
-		circulo := Circulo{10}
-		verificaArea(t, circulo, 314.1592653589793)
-
-	})
 }
